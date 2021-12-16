4ab8c12a32c02af183c5d60eec9d6a6cc3703611
Rocco P <roccopan@gmail.com>
2021-12-16 09:51:12 -0500

    split up, round 1

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
 import { $ } from './utils.js'
-
-// DATA
-const playlist = [
-  `audio/bensound-sunny.mp3`,
-  `https://cdn.pixabay.com/audio/2021/11/13/audio_cb4f1212a9.mp3`,
-  `https://cdn.pixabay.com/audio/2021/11/24/audio_82498b22da.mp3`,
-  `https://cdn.pixabay.com/audio/2021/11/25/audio_91b32e02f9.mp3`,
-  `https://cdn.pixabay.com/audio/2021/11/23/audio_035a943c87.mp3`,
-  `https://cdn.pixabay.com/audio/2021/11/01/audio_00fa5593f3.mp3`,
-  `https://cdn.pixabay.com/audio/2021/08/08/audio_dc39bde808.mp3`,
-]
+import { playlist } from './data.js'
 
 // DOCUMENT ELEMENTS
 const playOrPause = $(`#playOrPause`)
