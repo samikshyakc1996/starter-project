635f023cb4fc8a44cd9994d0c96a1f1b6f073e0d
Rocco P <roccopan@gmail.com>
2021-12-16 10:38:32 -0500

    loaded class

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { $ } from './utils.js'
+import { $, $$ } from './utils.js'
 import { playlist } from './data.js'
 
 // DOCUMENT ELEMENTS
@@ -12,9 +12,9 @@ let playingIndex = 0
 const song = new Audio()
 
 // FUNCTIONS
-const loadSongFromPlaylistByIndex = function(index = 0) {
+const loadSongFromPlaylistByIndex = function(index = 0, start = false) {
   // Assign the incoming index to the playlistIndex
-  playlistIndex = index
+  playingIndex = index
 
   // Check if the song was already playing (ie, not paused), so we can keep it going after changing the src
   const keepPlaying = !song.paused
@@ -24,22 +24,34 @@ const loadSongFromPlaylistByIndex = function(index = 0) {
 
   // Loading up the rest of the data for this song would go here
 
+  // Add .loading to the playing song
+  $$(`.playing`).forEach(li => li.classList.remove(`playing`))
+  $(`[data-index="${playingIndex}"]`).classList.add(`playing`)
+
   // Play the new track, if we were already playing (you may not even want to bother checking and just play when a playlist song is clicked)
-  if (keepPlaying) {
+  if (keepPlaying || start) {
     song.play()
   }
 }
 
+const loadPlaylistFromArray = function(pl) {
+  pl.innerHTML = ``
+  // Load up the playlist
+  pl.forEach(function(item, index) {
+    playlistEle.innerHTML += `<li data-index="${index}">${item}</li>`
+  })
+}
+$(`#loadPlaylist`).addEventListener(`click`, () => loadPlaylistFromArray(playlist))
+
+
 // START THE APP
 window.addEventListener(`load`, function() {
 
-  // Setup the first song to play
-  song.src = playlist[playingIndex]
+  // Load up the playlist with songs
+  loadPlaylistFromArray(playlist)
 
-  // Load up the playlist
-  playlist.forEach(function(item, index) {
-    playlistEle.innerHTML += `<li data-index="${index}">${item}</li>`
-  })
+  // Setup the first song to play
+  loadSongFromPlaylistByIndex(playingIndex)
 
   // If play/pause is clicked
   playOrPause.addEventListener(`click`, function(event) {
@@ -68,15 +80,22 @@ window.addEventListener(`load`, function() {
     loadSongFromPlaylistByIndex(prevIndex)
   })
 
+  // Add a listener to the entire playlist
   playlistEle.addEventListener(`click`, function(event) {
     // Who was clicked?
     const songToPlay = event.target
 
     if (songToPlay.matches(`li`)) {
       playingIndex = Number(songToPlay.dataset.index)
-      song.src = playlist[playingIndex]
-      song.play()
+      loadSongFromPlaylistByIndex(playingIndex, true)
     }
   })
 })
 
+// BREAK UNTIL 10:45
+// - Any questions about logic?
+// - Anything you want me to demo?
+// - Anything you want to show?
+// - Wrapup:
+//    - Submission
+//    - Quiz
