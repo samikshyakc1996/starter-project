2633abf0f3e6a388950d559ba8d4d9bad5bdc299
samikshyakc1996 <samikshyakc1996@gmail.com>
2022-04-05 17:36:25 -0400

    minor change 2

--- starter-project/src/App.js
+++ starter-project/src/App.js
@@ -1,25 +1,36 @@
-import logo from "./logo.svg";
-import "./App.css";
-
+// import { Link, Routes, Route } from "react-router-dom";
+// import About from "./components/About.jsx";
+// import Home from "./components/Home.jsx";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>hi just to see if this is working!</p>
-      </header>
-    </div>
+    <header>
+      <div>
+        {/* <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav> */}
+      </div>
+      <p>hi just to see if this is working!</p>
+      <h1>hi just to see if this is working! in h1</h1>
+
+      {/* <Routes>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+      </Routes> */}
+    </header>
   );
 }
 
